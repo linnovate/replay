6388f6d2bd5d89b9bda35753bd302100404742f6
Alexander Borisenko <wealthybora@gmail.com>
2016-04-14 13:28:14 +0300

    added component feature1

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,9 +1,11 @@
 import angular from 'angular';
 import Home from './home/home';
+import Feature1 from './feature1/feature1';
 import About from './about/about';
 
 let componentModule = angular.module('app.components', [
   Home.name,
+  Feature1.name,
   About.name
 ]);
 
---
+++ client/app/components/feature1/feature1.controller.js
@@ -0,0 +1,8 @@
+class Feature1Controller {
+  constructor() {
+    this.name = 'feature1';
+    this.cities = ['New York', 'San Francisco', 'Tel-Aviv', 'Los Angeles'];
+  }
+}
+
+export default Feature1Controller;
