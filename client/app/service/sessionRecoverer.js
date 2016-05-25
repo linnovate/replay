6956ea56ed34bcb3f6bad59a1cfa1dc9d33f38f6
Alexander Borisenko <wealthybora@gmail.com>
2016-05-25 18:30:43 +0300

    remove logs

--- client/app/service/sessionRecoverer.js
+++ client/app/service/sessionRecoverer.js
@@ -3,10 +3,8 @@ export default function SessionRecoverer($q, $window, $injector) {
 
   return {
     responseError: function (response) {
-      console.log('response', response);
       // Session has expired
       if (response.status == 401 && gapi && gapi.auth2.getAuthInstance().currentUser.get().isSignedIn()){
-        console.log('inside responseError!');
         var $http = $injector.get('$http');
 
         $window.sessionStorage.id_token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().id_token;
