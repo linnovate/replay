c06391c1e3f2db33539f9b7d47107e998444db6b
Alexander Borisenko <wealthybora@gmail.com>
2016-09-14 13:33:15 +0300

    added new confirm method, added jsdoc

--- client/app/service/dialog.service.js
+++ client/app/service/dialog.service.js
@@ -6,7 +6,13 @@ export default class DialogService {
     this.$mdDialog = $mdDialog;
   }
 
-    showAlert(msg, title = 'Interesting fact...', btnCaption = 'OK') {
+  /**
+   * Show alert dialog
+   * @param msg
+   * @param title
+   * @param btnCaption
+   */
+  showAlert(msg, title = 'Interesting fact...', btnCaption = 'OK') {
     this.$mdDialog.show(
       this.$mdDialog.alert()
         .parent(angular.element(document.getElementById('map-main')))
@@ -18,6 +24,16 @@ export default class DialogService {
     );
   }
 
+  /**
+   * Show prompt dialog
+   * @param title
+   * @param content
+   * @param placeholder
+   * @param defalutVal
+   * @param btnOk
+   * @param btnCancel
+   * @returns {*|void}
+   */
   showPrompt(title, content = '', placeholder = '', defalutVal = '', btnOk = 'OK', btnCancel = 'Cancel') {
     var confirm = this.$mdDialog.prompt()
       .title(title)
@@ -25,6 +41,25 @@ export default class DialogService {
       .placeholder(placeholder)
       .ariaLabel(placeholder)
       .initialValue(defalutVal)
+      .ok(btnOk)
+      .cancel(btnCancel);
+
+    return this.$mdDialog.show(confirm);
+  }
+
+  /**
+   * Show confirm dialog
+   * @param title
+   * @param content
+   * @param ariaLabel
+   * @param btnOk
+   * @param btnCancel
+   */
+  showConfirm(title, content = '', ariaLabel = '', btnOk = 'OK', btnCancel = 'Cancel') {
+    var confirm = this.$mdDialog.confirm()
+      .title(title)
+      .textContent(content)
+      .ariaLabel(ariaLabel)
       .ok(btnOk)
       .cancel(btnCancel);
 
